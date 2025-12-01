23f232053df4d5d8d5bb5ef8f29a1c253799897b
Shreyansh Gupta <hardikshrey123@gmail.com>
2025-12-02 00:41:19 +0530

    Add text animation component with character splitting and reset functionality

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,29 @@
 import { Text, View } from "react-native";
+import { useState } from "react";
+import { Button } from "@react-navigation/elements";
+
+function text(text: string) {
+  return (
+    <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
+      {text.split("").map((char, i) => (
+        <Text key={i}>{char === " " ? "\u00A0" : char}</Text>
+      ))}
+    </View>
+  );
+}
 
 export default function Index() {
+  const [reset, setReset] = useState(0);
+  const animationText = "Animations";
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <View key={reset}>
+        {text(animationText)}
+        {text(animationText)}
+      </View>
+
+      <Button onPress={() => setReset(reset + 1)}>Reset</Button>
     </View>
   );
 }
