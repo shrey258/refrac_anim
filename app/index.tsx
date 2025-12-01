ea8d57cf0a94153442f7de1e7e242c802c236216
Shreyansh Gupta <hardikshrey123@gmail.com>
2025-12-02 01:43:26 +0530

    Replace Button with Pressable and refactor styling for better React Native compatibility

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,5 @@
-import { Button } from "@react-navigation/elements";
 import { useState } from "react";
-import { StyleSheet, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 import Animated, { cubicBezier } from "react-native-reanimated";
 
 const fadein = {
@@ -16,13 +15,21 @@ const fadein = {
 
 function text(text: string, inverted: boolean) {
   return (
-    <View style={[styles.h1, inverted && { 
-      transform: [{ rotate: '180deg' }, { rotateY: '180deg' }],
-      opacity: 0.3,
-      backdropFilter: 'blur(20px) saturate(160%)'
-    }]}>
+    <View
+      style={[
+        styles.h1,
+        inverted && {
+          transform: [{ rotate: "180deg" }, { rotateY: "180deg" }],
+          opacity: 0.3,
+          backdropFilter: "blur(20px) saturate(160%)",
+        },
+      ]}
+    >
       {text.split("").map((char, i) => (
-        <Animated.Text style={[styles.letter, { animationDelay: `${0.03 * i}s` }]} key={i}>
+        <Animated.Text
+          style={[styles.letter, { animationDelay: `${0.03 * i}s` }]}
+          key={i}
+        >
           {char === " " ? "\u00A0" : char}
         </Animated.Text>
       ))}
@@ -41,9 +48,9 @@ export default function Index() {
         {text(animationText, true)}
       </View>
 
-      <Button onPress={() => setReset(reset + 1)} style={[styles.button]}>
-        Reset
-      </Button>
+      <Pressable onPress={() => setReset(reset + 1)} style={styles.button}>
+        <Text style={styles.buttonText}>Replay animation</Text>
+      </Pressable>
     </View>
   );
 }
@@ -52,13 +59,13 @@ const styles = StyleSheet.create({
   h1: {
     flexDirection: "row",
     flexWrap: "wrap",
-    fontSize: 32,
-    fontWeight: 600,
-    letterSpacing: -0.05,
     overflow: "hidden",
   },
 
   letter: {
+    fontSize: 32,
+    fontWeight: "600",
+    letterSpacing: -0.05,
     transform: [{ translateY: "100%" }],
     animationName: fadein,
     animationDuration: "1.3s",
@@ -66,15 +73,18 @@ const styles = StyleSheet.create({
     animationTimingFunction: cubicBezier(0.19, 1, 0.22, 1) as any,
   },
   button: {
-    width: 100,
+    width: "100%",
+    maxWidth: 140, // constrained width for better look
     marginTop: 24,
-    position: "relative",
     height: 32,
-    fontSize: 14,
-    paddingInline: 12,
-    fontWeight: 500,
+    justifyContent: "center",
+    alignItems: "center",
     borderRadius: 9999,
     backgroundColor: "#FFF",
-    boxShadow: "0 0 0 1px rgba(0, 0, 0, 0.08), 0px 2px 2px rgba(0, 0, 0, 0.04)",
+  },
+  buttonText: {
+    fontSize: 14,
+    fontWeight: "500",
+    color: "black",
   },
 });
